c3b416b4cfb04883cc288d79737f6b1d8680deef
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-11-19 20:48:02 +0200

    Claude: fix prefill tool calling hack with at-depth caching

--- src/endpoints/backends/chat-completions.js
+++ src/endpoints/backends/chat-completions.js
@@ -130,10 +130,6 @@ async function sendClaudeRequest(request, response) {
             delete requestBody.system;
         }
         if (useTools) {
-            // Claude doesn't do prefills on function calls, and doesn't allow empty messages
-            if (convertedPrompt.messages.length && convertedPrompt.messages[convertedPrompt.messages.length - 1].role === 'assistant') {
-                convertedPrompt.messages.push({ role: 'user', content: '.' });
-            }
             additionalHeaders['anthropic-beta'] = 'tools-2024-05-16';
             requestBody.tool_choice = { type: request.body.tool_choice };
             requestBody.tools = request.body.tools
@@ -141,6 +137,10 @@ async function sendClaudeRequest(request, response) {
                 .map(tool => tool.function)
                 .map(fn => ({ name: fn.name, description: fn.description, input_schema: fn.parameters }));
 
+            // Claude doesn't do prefills on function calls, and doesn't allow empty messages
+            if (requestBody.tools.length && convertedPrompt.messages.length && convertedPrompt.messages[convertedPrompt.messages.length - 1].role === 'assistant') {
+                convertedPrompt.messages.push({ role: 'user', content: [{ type: 'text', text: '\u200b' }] });
+            }
             if (enableSystemPromptCache && requestBody.tools.length) {
                 requestBody.tools[requestBody.tools.length - 1]['cache_control'] = { type: 'ephemeral' };
             }
