da42d0a47a1a2ca8206ebb95cf408596973fd421
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-11-23 19:39:02 +0200

    Add 3-state find substitution select Fixes #3104

--- public/scripts/char-data.js
+++ public/scripts/char-data.js
@@ -89,7 +89,7 @@
 * @property {boolean} markdownOnly - Whether the script only applies to Markdown
 * @property {boolean} promptOnly - Whether the script only applies to prompts
 * @property {boolean} runOnEdit - Whether the script runs on edit
-* @property {boolean} substituteRegex - Whether the regex should be substituted
+* @property {number} substituteRegex - Whether the regex should be substituted
 * @property {number} minDepth - The minimum depth
 * @property {number} maxDepth - The maximum depth
 */
