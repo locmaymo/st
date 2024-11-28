f630c8892a48b3970fd8b38a8edb64333d399d88
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-11-28 22:39:20 +0200

    Webpack: cache lib.js to disk to prevent occasional OOM

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ export const publicLibConfig = {
         hints: false,
     },
     output: {
+        path: path.resolve(process.cwd(), 'dist'),
         filename: 'lib.js',
         libraryTarget: 'module',
     },
