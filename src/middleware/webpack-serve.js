49fb2f4d792cc2902c84f3706ccfe2adcc5d8d9e
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-01-07 20:24:57 +0200

    Declare compiler in the inner scope #3272

--- src/middleware/webpack-serve.js
+++ src/middleware/webpack-serve.js
@@ -5,8 +5,6 @@ import { publicLibConfig } from '../../webpack.config.js';
 export default function getWebpackServeMiddleware() {
     const outputPath = publicLibConfig.output?.path;
     const outputFile = publicLibConfig.output?.filename;
-    /** @type {import('webpack').Compiler|null} */
-    let compiler = webpack(publicLibConfig);
 
     /**
      * A very spartan recreation of webpack-dev-middleware.
@@ -28,12 +26,9 @@ export default function getWebpackServeMiddleware() {
      * @returns {Promise<void>}
      */
     devMiddleware.runWebpackCompiler = () => {
-        return new Promise((resolve) => {
-            if (compiler === null) {
-                console.warn('Webpack compiler is already closed.');
-                return resolve();
-            }
+        const compiler = webpack(publicLibConfig);
 
+        return new Promise((resolve) => {
             console.log();
             console.log('Compiling frontend libraries...');
             compiler.run((_error, stats) => {
@@ -42,11 +37,7 @@ export default function getWebpackServeMiddleware() {
                     console.log(output);
                     console.log();
                 }
-                if (compiler === null) {
-                    return resolve();
-                }
                 compiler.close(() => {
-                    compiler = null;
                     resolve();
                 });
             });
